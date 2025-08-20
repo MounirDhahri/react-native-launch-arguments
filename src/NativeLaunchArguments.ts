f306f8f1d52cf500e8258570e310bc40ded1238c
Mounir Dhahri <soussouheros@gmail.com>
2025-08-20 17:40:03 +0200

    chore: support new architecture
    chore: follow-up fixes
    
    chore: upgrade packages and fix typings

---
+++ src/NativeLaunchArguments.ts
@@ -0,0 +1,10 @@
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+
+export interface Spec extends TurboModule {
+  readonly getConstants: () => {
+    readonly value: { [key: string]: string };
+  };
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('LaunchArguments');
