54fa25b4bfcf1ff3a52e1ba8c88b2e284c15fd3e
Mounir Dhahri <soussouheros@gmail.com>
2025-12-08 16:52:54 +0100

    feat: add new architecture support to android

--- android/src/main/java/com/reactnativelauncharguments/LaunchArgumentsPackage.java
+++ android/src/main/java/com/reactnativelauncharguments/LaunchArgumentsPackage.java
@@ -1,23 +1,45 @@
 package com.reactnativelauncharguments;
 
-import com.facebook.react.ReactPackage;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.module.model.ReactModuleInfo;
+import com.facebook.react.module.model.ReactModuleInfoProvider;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class LaunchArgumentsPackage implements ReactPackage {
-    
+public class LaunchArgumentsPackage extends TurboReactPackage {
+
+    @Nullable
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+    public NativeModule getModule(@NonNull String name, @NonNull ReactApplicationContext reactContext) {
+        if (name.equals(LaunchArgumentsModule.NAME)) {
+            return new LaunchArgumentsModule(reactContext);
+        } else {
+            return null;
+        }
     }
 
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        final NativeModule nativeModule = new LaunchArgumentsModule(reactContext);
-        return Collections.singletonList(nativeModule);
+    public ReactModuleInfoProvider getReactModuleInfoProvider() {
+        return () -> {
+            final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+            moduleInfos.put(
+                    LaunchArgumentsModule.NAME,
+                    new ReactModuleInfo(
+                            LaunchArgumentsModule.NAME,
+                            LaunchArgumentsModule.NAME,
+                            false, // canOverrideExistingModule
+                            false, // needsEagerInit
+                            true, // hasConstants
+                            false, // isCxxModule
+                            true // isTurboModule
+                    ));
+            return moduleInfos;
+        };
     }
 }
